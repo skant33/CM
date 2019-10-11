b19b6119dd25030419e0fde7cfa2276000c9aa3c
Steven Kant <stevenkant@gmail.com>
2019-10-11 10:54:27 +0200

    Standaard project aangemaakt

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
