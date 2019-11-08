de79257a63475478f3e72912e8beaed95df3a711
Rienk Engbrenghof <engbrenghof48@gmail.com>
2019-11-08 13:03:43 +0100

    CM

--- SDK/Common/Constant.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CM.Text.Common
-{
-    internal static class Constant
-    {
-        internal static readonly string TextSdkReference = "text-sdk-dotnet-" +
-                                                           typeof(TextClient).Assembly.GetName()
-                                                               .Version;
-    }
-}
