99a15fb5bb539c863dce2d0d6e7db39e7551ed41
Rienk Engbrenghof <engbrenghof48@gmail.com>
2019-11-20 13:13:34 +0100

    CM

--- Controllers/NotificationController.cs
+++ Controllers/NotificationController.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using CM.Text;
 using CM.Voice.VoiceApi.Sdk;
+using CM.Voice.VoiceApi.Sdk.Models;
 using CM.Voice.VoiceApi.Sdk.Models.Instructions.Apps;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -15,6 +16,7 @@ namespace CM.Controllers
     public class NotificationController : Controller
     {
         private IConfiguration config;
+
         public NotificationController(IConfiguration config)
         {
             this.config = config;
@@ -27,29 +29,30 @@ namespace CM.Controllers
 
         public async Task<IActionResult> SendSMS()
         {
-            List<string> receivers = new List<string>();
-            receivers.Add("0031627404177");
+            //List<string> receivers = new List<string>();
+            //receivers.Add("0031627404177");
 
-            var client = new TextClient(new Guid(config.GetSection("ApiKey").Value));
-            var result = await client.SendMessageAsync("Hoi", "CMProftaak", receivers, null).ConfigureAwait(false);
-           
-            return View("~/Views/Home/Login.cshtml");
+            //var client = new TextClient(new Guid(config.GetSection("ApiKey").Value));
+            //var result = await client.SendMessageAsync("Hoi", "CMProftaak", receivers, null).ConfigureAwait(false);
+
+            await SendPhoneConversation();
+            return View("~/Views/Home/Login.cshtml");            
         }
 
-        public async Task<IActionResult> SendPhoneConversation()
+        public async Task SendPhoneConversation()
         {
-            var myApiKey = Guid.Parse(config.GetSection("X-CM-PRODUCTTOKEN").Value);
+            var myApiKey = Guid.Parse("E4802F51-F6A2-474A-8883-3CDB2EAACDB3");
             var httpClient = new HttpClient();
-            var client = new VoiceApiClient(httpClient, myApiKey);            
+            var client = new VoiceApiClient(httpClient, myApiKey);
             var instruction = new NotificationInstruction
             {
                 Caller = "0031627404177",
                 Callee = "0031634698094",
-                Prompt = "hoi max je bent echt lelijk, ga maar goed werken kehba",
+                Prompt = "This is a test notification call using the CM voice A P I.",
+                MaxReplays = 2,
+                ReplayPrompt = "Press 1 to repeat this message."
             };
             var result = await client.SendAsync(instruction).ConfigureAwait(false);
-            return View("~/Views/Home/Login.cshtml");
-
         }
     }
 }
