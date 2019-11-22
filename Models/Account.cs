f51c8d6586c0f3749ccc8933bff95ea55c43b2a4
Max <maxvdd76@gmail.com>
2019-11-22 13:42:10 +0100

    AccountSqlFix

--- Models/Account.cs
+++ Models/Account.cs
@@ -9,10 +9,11 @@ namespace CM.Models
     {
         public int AccountID { get; set; }
         public string Password { get; set; }
-        public AccountRole  RoleId { get; set; }
         public string Name { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
         public int PhoneNumber { get; set; }
+
+        public AccountRole RoleId = new AccountRole();
     }
 }
